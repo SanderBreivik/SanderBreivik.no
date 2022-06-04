6f9e7a19e97a4eb5a0d89b2c2b203860a96b1a58
SanderBreivik <s.b@live.no>
2022-06-04 10:06:39 +0200

    📦 NEW: add gtag and ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "../styles/globals.scss";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 
 function MyApp({ Component, pageProps }) {
+  ReactGA.initialize("G-J2FKZSW4WH");
+  ReactGA.send("pageview");
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-KSF7MRH" });
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
