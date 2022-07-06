9ab2904f6408b9313b95ccf20f89c7746d4c2252
SanderBreivik <s.b@live.no>
2022-07-06 11:43:53 +0200

    📦 NEW: Add resume and favicon and reduce image size

--- components/Features.tsx
+++ components/Features.tsx
@@ -16,7 +16,7 @@ const Features = () => {
   React.useEffect(() => {
     const intervalId = setInterval(
       () => setIndex((index) => index + 1),
-      3000 // every 3 seconds
+      2000 // every 3 seconds
     );
     return () => clearTimeout(intervalId);
   }, []);
