337d1734b0ab6f844f4b47c838f9dd55aadce772
SanderBreivik <s.b@live.no>
2024-07-29 19:41:15 +0200

    Update favicon links and manifest

--- pages/index.tsx
+++ pages/index.tsx
@@ -128,7 +128,10 @@ export default function Home() {
             name="description"
             content="Sander Breiviks personal full-stack profile"
           />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png"/>
+          <link rel="manifest" href="/favicon/site.webmanifest"/>
           <title>Sander Brevik</title>
         </Head>
         <CookieConsent buttonStyle={{ color: "black", background: "white" }}>
